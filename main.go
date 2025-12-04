5f4c5b0100b5e6534a4d8110c0ce4475c16ac991
yurifws <yurifws@gmail.com>
2025-12-04 00:16:47 -0300

    <feat GPL-027> joining a slice of strings

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 
 }
