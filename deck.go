2e01e311d83b1a570d16e0a5afc9b44f62550ec5
yurifws <yurifws@gmail.com>
2025-12-04 11:45:59 -0300

    <feat GPL-031> shuffling a deck

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -53,4 +54,14 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my_card")
+	cards := newDeck()
 
+	cards.shuffle()
 	cards.print()
 }
